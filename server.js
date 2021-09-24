8a8cde2d75f7de6217b3c6ae740e03a9ba187e94
abmohamm <mohd.92java@gmail.com>
2021-09-24 14:48:08 -0400

    Removed inline-html

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 /*jshint esversion: 6 */
-
 const express = require('express');
+const { dirname } = require('path');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log('PORT : ' || process.env.PORT);
-    console.log(`Listening on Port : ${PORT}`);
+    console.log('<============================ PORT : ' + process.env.PORT + ' ============================>');
+    console.log(`<============================ Listening on Port : ${PORT} ============================>`);
+    console.log('<============================ current path : ' + path.resolve(__dirname) + " ============================>");
 });
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello!!! Welcome to Express JS Programming Tutorial</h1>');
+    response.send('');
 });
