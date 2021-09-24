ec337720a57b9d21621aaa04aa8d59eed05ce221
abmohamm <mohd.92java@gmail.com>
2021-09-24 16:13:43 -0400

    refactoring changes

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 /*jshint esversion: 6 */
 const express = require('express');
+
 const { dirname } = require('path');
+
 const path = require('path');
+
 const app = express();
+
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
 app.listen(PORT, () => {
     console.log('<============================ PORT : ' + process.env.PORT + ' ============================>');
     console.log(`<============================ Listening on Port : ${PORT} ============================>`);
