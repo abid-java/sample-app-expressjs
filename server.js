1485989c7005217564667ddbbdca9c74b2e4b8d6
abmohamm <mohd.92java@gmail.com>
2021-09-24 13:17:11 -0400

    PORT changes

--- server.js
+++ server.js
@@ -2,5 +2,8 @@
 
 const express = require('express');
 const app = express();
-
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log('PORT : ' || process.env.PORT);
+    console.log(`Listening on Port : ${PORT}`);
+});
