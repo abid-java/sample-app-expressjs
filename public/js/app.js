cbb1f081a25413924cba897ab62c975a75f60e03
abmohamm <mohd.92java@gmail.com>
2021-09-27 16:08:31 -0400

    Changing attributes in React Component

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,13 @@
+/*jshint esversion: 6 */
+/*jshint -W030 */
 const App = () => {
     return (
         <ul class="list-group">
-            <li class="list-group-item active" aria-current="true">An active item</li>
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
-            <li class="list-group-item">A fourth item</li>
-            <li class="list-group-item">And a fifth one</li>
+            <li className="list-group-item active" aria-current="true">An active item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+            <li className="list-group-item">A fourth item</li>
+            <li className="list-group-item">And a fifth one</li>
         </ul>
     )
 };
