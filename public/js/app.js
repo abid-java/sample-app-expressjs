2566a2940007776344a892d9c8ad2fca0a880a8d
abmohamm <mohd.92java@gmail.com>
2021-09-27 15:29:00 -0400

    Displaying data list

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,11 @@
-/*jshint esversion: 6 */
-/*jshint -W030 */
 const App = () => {
     return (
-        <ul className="list-group">
-            <li className="list-group-item active" aria-current="true">An active item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
+        <ul class="list-group">
+            <li class="list-group-item active" aria-current="true">An active item</li>
+            <li class="list-group-item">A second item</li>
+            <li class="list-group-item">A third item</li>
+            <li class="list-group-item">A fourth item</li>
+            <li class="list-group-item">And a fifth one</li>
         </ul>
     )
 };
